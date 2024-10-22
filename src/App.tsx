c13de17f7c86e923bdd06fd7824521171c451efd
delucianina <delucianina@gmail.com>
2024-10-22 15:02:30 -0400

    initial commit and push

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+function Header() {
+  return (
+    <header>
+      <h3>Logo</h3>
+    </header>
+  );
+}
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <Header />
+      <h1>First React Header</h1>
+      <p>Some paragraph text below</p>
+    </>
+  );
+}
+
+export default App
